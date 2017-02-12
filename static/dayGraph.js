9ed93ea850a2c56a025e30103af31fd18f9e0347
aga3134 <aga3134@gmail.com>
2017-02-12 20:14:00 +0800

    rearrange day graph

--- static/dayGraph.js
+++ static/dayGraph.js
@@ -45,7 +45,8 @@ function DrawDayGraph(sensorData) {
   dayContainer.html("");
   var w = Math.min($("body").width(), dayContainer.width());
   var offsetX = (w-53*cellSize)*0.5;
-  if(offsetX < 30) offsetX = 30;
+  if(offsetX < 40) offsetX = 40;
+  var offsetY = cellSize;
   
   var year = parseInt($("#showYear").val());
   var startDate = new Date(year,0,1);
@@ -64,8 +65,14 @@ function DrawDayGraph(sensorData) {
     html.attr("onmouseover","ShowDate('"+day+"');");
     html.attr("onmouseout","ShowDate();");
     html.css("border","1px solid #EEE");
-    html.css("top",weekDay*cellSize);
-    html.css("left",(w-1)*cellSize+offsetX);
+
+    //html.css("top",weekDay*cellSize);
+    //html.css("left",(w-1)*cellSize+offsetX);
+    html.css("top",m*cellSize+offsetY+2);
+    var firstWeekDay = new Date(year,m,1);
+    var coordX = d.getDate()+firstWeekDay.getDay();
+    html.css("left",coordX*cellSize+offsetX);
+
     var avg = sensorData[day];
     if(avg){
       html.css("background","linear-gradient("+color(avg.northAvg)+","+color(avg.centralAvg)+","+color(avg.southAvg)+")");
@@ -76,13 +83,13 @@ function DrawDayGraph(sensorData) {
     }
     
     //add month border
-    var nl = AddDate(d,-7), nr = AddDate(d,7);
+    /*var nl = AddDate(d,-7), nr = AddDate(d,7);
     var nt = AddDate(d,-1), nb = AddDate(d,1);
     var borderStyle = "2px solid blue";
     if(nr.getMonth() != m) html.css("border-right",borderStyle);
     if(nt.getMonth() != m || weekDay == 0) html.css("border-top",borderStyle);
     if(nl.getMonth() == 11 && m != 11) html.css("border-left",borderStyle);
-    if(weekDay == 6 || (m == 11 && nb.getMonth() != 11)) html.css("border-bottom",borderStyle);
+    if(weekDay == 6 || (m == 11 && nb.getMonth() != 11)) html.css("border-bottom",borderStyle);*/
     dayContainer.append(html);
   }
   //add select block
@@ -96,14 +103,24 @@ function DrawDayGraph(sensorData) {
   for(var i=0;i<12;i++){
     var html = $("<div>"+(i+1)+"月</div>");
     html.attr("class","time-label");
-    html.css("top",cellSize*7);
-    html.css("left",offsetX+(i)*53*cellSize/12);
+    //html.css("top",cellSize*7);
+    //html.css("left",offsetX+(i)*53*cellSize/12);
+    html.css("top",cellSize*i+offsetY);
+    html.css("left",0);
+    dayContainer.append(html);
+
+    html = $("<div>"+(i+1)+"月</div>");
+    html.attr("class","time-label");
+    html.css("top",cellSize*i+offsetY);
+    html.css("left",(7*6)*cellSize+offsetX);
     dayContainer.append(html);
   }
   //add weekday label
-  for(var i=0;i<7;i++){
+  //for(var i=0;i<7;i++){
+  for(var i=0;i<7*6;i++){
     var wd = "";
-    switch(i){
+    //switch(i){
+    switch(i%7){
       case 0: wd = "日"; break;
       case 1: wd = "一"; break;
       case 2: wd = "二"; break;
@@ -114,74 +131,16 @@ function DrawDayGraph(sensorData) {
     }
     var html = $("<div>"+wd+"</div>");
     html.attr("class","time-label");
-    html.css("top",i*cellSize);
-    html.css("left",offsetX-20);
+    //html.css("top",i*cellSize);
+    //html.css("left",offsetX-20);
+    html.css("top",0);
+    html.css("left",i*cellSize+offsetX);
     dayContainer.append(html);
 
-    var html = $("<div>"+wd+"</div>");
+    /*var html = $("<div>"+wd+"</div>");
     html.attr("class","time-label");
     html.css("top",i*cellSize);
     html.css("left",offsetX+cellSize*53+5);
-    dayContainer.append(html);
+    dayContainer.append(html);*/
   }
-/*
-  var day = d3.time.format("%w"),
-    week = d3.time.format("%U"),
-    format = d3.time.format("%Y/%m/%d");
-
-  var svg = d3.select("#dayGraph");
-
-  //setup color gradient by data
-  var defs = svg.append("defs");
-  var gradient = defs.selectAll("linearGradient")
-    .data(function(d) { return d3.time.days(new Date(2016, 0, 1), new Date(2017, 0, 1)); })
-    .enter().append("linearGradient");
-    
-  gradient.attr("id", function(d,i){return i;})
-    .attr("x1", "0%").attr("x2", "0%")
-    .attr("y1", "0%").attr("y2","100%");
-  gradient.append("stop")
-    .attr("offset","0%")
-    .attr("stop-color", function(d){return color(day(d)*1);});
-  gradient.append("stop")
-    .attr("offset","50%")
-    .attr("stop-color", function(d){return color(day(d)*5);});
-  gradient.append("stop")
-    .attr("offset","100%")
-    .attr("stop-color", function(d){return color(day(d)*10);});
-
-  //draw rect per day
-  var rect = svg.selectAll(".day")
-        .data(function(d) { return d3.time.days(new Date(2016, 0, 1), new Date(2017, 0, 1)); })
-      .enter().append("rect")
-        .attr("class", "day")
-        .attr("data-date", function(d){return d;})
-        .attr("width", cellSize)
-        .attr("height", cellSize)
-        .attr("x", function(d) { return week(d) * cellSize; })
-        .attr("y", function(d) { return day(d) * cellSize; })
-        .style("fill", function(d,i){return "url(#"+i+")";})
-        .style("stroke", "red")
-        .datum(format);
-
-  //draw outline per month
-  svg.selectAll(".month")
-        .data(function(d) { return d3.time.months(new Date(2016, 0, 1), new Date(2017, 0, 1)); })
-      .enter().append("path")
-        .attr("class", "month")
-        .attr("d", monthPath)
-        .style("fill", "none")
-        .style("stroke-width", "3")
-        .style("stroke", "black");
-
-  function monthPath(t0) {
-    var t1 = new Date(t0.getFullYear(), t0.getMonth() + 1, 0),
-        d0 = +day(t0), w0 = +week(t0),
-        d1 = +day(t1), w1 = +week(t1);
-    return "M" + (w0 + 1) * cellSize + "," + d0 * cellSize
-        + "H" + w0 * cellSize + "V" + 7 * cellSize
-        + "H" + w1 * cellSize + "V" + (d1 + 1) * cellSize
-        + "H" + (w1 + 1) * cellSize + "V" + 0
-        + "H" + (w0 + 1) * cellSize + "Z";
-  }*/
 }
