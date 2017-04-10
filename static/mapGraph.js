b7ef0354a69e16b4cead22e2597a4ba579d0dc47
aga3134 <aga3134@gmail.com>
2017-04-10 21:52:06 +0800

    fix bug: pm2.5 color turn white when larger than 70

--- static/mapGraph.js
+++ static/mapGraph.js
@@ -56,7 +56,7 @@ function rgb(r,g,b){
 }
 
 function ValueToColor(v){
-	var color = d3.scale.linear().domain([0,11,23,35,41,47,53,58,64,70,71])
+	var color = d3.scale.linear().domain([0,11,23,35,41,47,53,58,64,70])
 		.range([rgb(156,255,156), rgb(49, 255, 0), rgb(49, 207, 0), rgb(255, 255, 0), rgb(255, 207, 0), 
 			rgb(255, 154, 0), rgb(255, 100, 100), rgb(255, 0, 0), rgb(153, 0, 0), rgb(206, 48, 255)]);
 	return color(v);
